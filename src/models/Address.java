0f17416c0ea0718fecbaa7cfd50668938e087e25
faisal98m <faisal.maroof791@gmail.com>
2025-02-10 20:04:04 +0000

    Class Responsibilities

--- src/models/Address.java
+++ src/models/Address.java
@@ -1,5 +1,26 @@
 package models;
 
 public class Address {
+
+/*
+ * Represents a patient's address. Stores and manges address details for pt's
+ * 
+ * Responsibility:
+ * -Store address information
+ * -Retrieve and update address
+ * -Format address for displau
+ * 
+ * Not responsible for:
+ * -Searching for a patient by address
+ * -Verifying address validity
+ * 
+ * 
+ * Attributes:
+ * houseNumber / streetname / city / postcode
+ * 
+ * Methods:
+ * -> Getters and Setters
+ */
+
     
 }
